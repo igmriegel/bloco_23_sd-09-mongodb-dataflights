40ce58c5257678e4e1762fac7760ba9ca3d5e9e3
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-06-07 20:24:02 -0300

    Desafio 28

---
+++ challenges/desafio28.js
@@ -0,0 +1,7 @@
+const empresa = "LATAM AIRLINES BRASIL";
+const totalVoosDomesticos = db.voos.find({
+  $and: [{ "empresa.nome": empresa }, { natureza: "Doméstica" }] }).count();
+
+db.resumoVoos.insertOne({ empresa, totalVoosDomesticos });
+
+db.resumoVoos.find({}, { _id: 0 });
